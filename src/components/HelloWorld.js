2e01ffe81ad321dbec761f44bd13bc036945748f
Kate <kate@kateandbenjy.com>
2021-04-26 18:30:26 +0100

    Update HelloWorld.js

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 import '../index.css';
 
 class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({ date: new Date() })
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.props.name}</h1>
+        <h1>Hello {this.props.name}</h1>
+        <div>It is {this.state.date.toLocaleTimeString()}</div>
         <div className="line-between-components"></div>
       </div>
     );
