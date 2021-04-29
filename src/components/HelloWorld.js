7f01a3507dd0954251a382f93ec420930e4d0411
Kate <kate@kateandbenjy.com>
2021-04-29 22:12:49 +0100

    Practise lists & keys

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,13 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../index.css';
 
-class HelloWorld extends React.Component {
+const n = [1,2,3,4,5];
 
+function ListItem(props) {
+  return (<li>{props.value}</li>);
+}
+
+function NumberList(props) {
+  return (
+      <ul>
+        {props.numbers.map((number) =>
+          <ListItem key={number.toString()} value={number} />
+        )}
+    </ul>
+  );
+}
+
+class HelloWorld extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello {this.props.name}</h1>
-        
+
+        <NumberList numbers={n} />
+
         <div className="line-between-components"></div>
       </div>
     );
