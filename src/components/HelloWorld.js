976bd2e1b30f6170856baebf83fc48164407b4b4
Kate <kate@kateandbenjy.com>
2021-04-30 19:14:46 +0100

    Try out controlled components

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -19,12 +19,59 @@ function NumberList(props) {
 }
 
 class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {v1: '', v2: '', v3: ''};
+
+    this.handleV1Change = this.handleV1Change.bind(this);
+    this.handleV2Change = this.handleV2Change.bind(this);
+    this.handleV3Change = this.handleV3Change.bind(this);
+  }
+
+  handleV1Change(e) {
+    this.setState({v1: e.target.value});
+  }
+
+  handleV2Change(e) {
+    this.setState({v2: e.target.value});
+  }
+
+  handleV3Change(e) {
+    this.setState({v3: e.target.value});
+  }
+
   render() {
     return (
       <div>
         <h1>Hello {this.props.name}</h1>
 
+        <div className="line-between-components"></div>
+
         <NumberList numbers={n} />
+
+        <div className="line-between-components"></div>
+
+        <form>
+          <div className="mt">
+            <input type="text" value={this.state.v1} onChange={this.handleV1Change} />
+          </div>
+          <div className="mt">
+            <textarea value={this.state.v2} onChange={this.handleV2Change} />
+          </div>
+          <div className="mt">
+            <select value={this.state.v3} onChange={this.handleV3Change}>
+              <option value="a">A</option>
+              <option value="b">B</option>
+              <option value="c">C</option>
+            </select>
+          </div>
+        </form>
+
+        <div className="mt">
+          <div>v1: {this.state.v1}</div>
+          <div>v2: {this.state.v2}</div>
+          <div>v3: {this.state.v3}</div>
+        </div>
 
         <div className="line-between-components"></div>
       </div>
